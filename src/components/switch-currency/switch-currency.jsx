460888646472834eb4d2cc34120ff1fc4312795e
Pavel Alekseev <pav.alekseev@icloud.com>
2024-12-22 19:15:04 +1100

    add ability to switch currencies via SwithCurrency

--- src/components/switch-currency/switch-currency.jsx
+++ src/components/switch-currency/switch-currency.jsx
@@ -1,22 +1,24 @@
+import PropTypes from "prop-types";
+
 import Grid from "@mui/material/Grid2";
 import Stack from "@mui/material/Stack";
 import IconButton from "@mui/material/IconButton";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
 
-const SwitchCurrency = () => {
-  const handleSwitch = () => {
-    console.log("ok");
-  };
-
+const SwitchCurrency = ({ onClick }) => {
   return (
     <Grid size={{ xs: 12, sm: "auto" }}>
       <Stack justifyContent="center" alignItems="center" height="100%">
-        <IconButton color="primary" onClick={handleSwitch}>
+        <IconButton color="primary" onClick={onClick}>
           <CompareArrowsIcon sx={{ fontSize: 30 }} />
         </IconButton>
       </Stack>
     </Grid>
   );
+};
+
+SwitchCurrency.propTypes = {
+  onClick: PropTypes.func,
 };
 
 export default SwitchCurrency;
